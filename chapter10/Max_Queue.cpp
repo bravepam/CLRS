8aabe5cccc021e368112e480074b55394181dbcc
panyong <1372120340@qq.com>
2016-04-11 15:16:25 +0800

    update max_queue's impl

--- chapter10/Max_Queue.cpp
+++ chapter10/Max_Queue.cpp
@@ -1,11 +1,3 @@
-
-/*******************************************
-* Author: bravepam
-* E-mail:1372120340@qq.com
-* Blog:http://blog.csdn.net/zilingxiyue
-*******************************************
-*/
-
 /*
  *实现一个这样一个队列：
  *1、出队操作pop(),删除队首元素，并且返回；
@@ -32,50 +24,49 @@ template <typename T>
 class max_stack
 {//最大值栈
 private:
-	vector<T> s;//栈，用数组实现
-	int m_index;//存储最大值所在的索引
-	vector<int> max_index;//max_index[i]处存储的是s[0...i - 1]的最大值的索引，即上一次的最大值索引
+	vector<T> s;//值栈
 	int t;//栈顶
+	vector<int> max_index;//索引栈，存储历次最大值索引
+	int m_index_t;//索引栈栈顶
 public:
-	max_stack() :s(), max_index(), m_index(MIN_INDEX), t(MIN_INDEX){}
+	max_stack() :s(), max_index(), m_index_t(MIN_INDEX), t(MIN_INDEX){}
 	bool empty()const { return t == MIN_INDEX; }
 	T pop()
 	{
-		if (t == m_index)//如果出栈元素正好是目前的最大值
-			m_index = max_index[m_index];//则更新最大值为上一次，即在此之前所有元素的最大值
+		if (s[t] == s[max_index[m_index_t]])//如果出栈元素正好是目前的最大值
+			--m_index_t;
 		return s[t--];
 	}
 	T top()const { return s[t]; }
 	T max()const
 	{
-		if (m_index > MIN_INDEX)//若存在元素
-			return s[m_index];
+		if (m_index_t > MIN_INDEX)//若存在元素
+			return s[max_index[m_index_t]];
 		else return MIN_DATA;//否则
 	}
-	void push(const T & dt)
+	void push(const T &dt)
 	{
 		if (t == static_cast<int>(s.size()) - 1)
-		{//如果栈满
+		{//如果值栈满
 			s.resize(2 * (t + 1) + 1);
-			max_index.resize(2 * (t + 1) + 1);
 		}
 		s[++t] = dt;
 		if (dt > max())
 		{//如果新元素比最大值大
-			max_index[t] = m_index;//则记下前一次最大值索引，这里类似于前驱子图
-			m_index = t;//更新现在的最大值索引
+			if (m_index_t == static_cast<int>(max_index.size()) - 1)
+				max_index.resize(2 * (m_index_t + 1) + 1);
+			max_index[++m_index_t] = t;
 		}
-		else max_index[t] = MIN_INDEX;//否则
 	}
-	/*void print()const
+	void print()const
 	{
 		for (int i = 0; i <= t; ++i)
 			cout << s[i] << ' ';
 		cout << endl;
-		for (int i = 0; i <= t; ++i)
-			cout << max_index[i] << ' ';
+		for (int i = 0; i <= m_index_t; ++i)
+			cout << s[max_index[i]] << ' ';
 		cout << endl;
-	}*/
+	}
 };
 
 template <typename T>
@@ -86,6 +77,7 @@ class max_queue
 private://采用两个最大值栈实现
 	max_stack<T> A;//入队数据存储栈
 	max_stack<T> B;//出队时若B空则将A中数据全部压入B
+
 	T maxOfTwo(const T &x, const T& y)const { return x > y ? x : y; }
 public:
 	max_queue() = default;
@@ -101,26 +93,46 @@ public:
 		return B.pop();//出B栈
 	}
 	void push(const T & val) { A.push(val); }
-	/*void print()const
+	void print()const
 	{
 		A.print();
 		B.print();
-	}*/
+	}
 };
 
 int main()
 {
-	max_queue<int> ms;
+	/*max_queue<int> mq;
 	vector<int> vec = { 3, 5, 7, 78,1, 3, 5, 45 };
 	for (size_t i = 0; i != vec.size(); ++i)
-		ms.push(vec[i]);
-	cout << boolalpha << ms.empty() << endl;
-	while (!ms.empty())
+		mq.push(vec[i]);
+	cout << boolalpha << mq.empty() << endl;
+	while (!mq.empty())
 	{
-		cout << "max: " << ms.max() << endl;
-		cout << "pop: " << ms.pop() << endl;
+		cout << "max: " << mq.max() << endl;
+		cout << "pop: " << mq.pop() << endl;
 	}
-	cout << boolalpha << ms.empty() << endl;
+	cout << boolalpha << mq.empty() << endl;
+	system("pause");*/
+
+	max_queue<int> mq;
+	vector<int> vec1 = { 3, 7, 2, 6, 10, 78 };
+	for (int i = 0; i != vec1.size(); ++i)
+		mq.push(vec1[i]);
+	cout << mq.pop() << endl;
+	mq.print();
+
+	vector<int> vec2 = { 45, 3, 5, 1, 79, 34 };
+	for (int i = 0; i != vec2.size(); ++i)
+		mq.push(vec2[i]);
+	mq.print();
+
+	while (!mq.empty())
+	{
+		cout << "---------------" << endl;
+		cout << mq.pop() << endl;
+		mq.print();
+	}
 	system("pause");
 	return 0;
 }
